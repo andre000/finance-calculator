95719ba57b6da06f53beae2240b3ea41b5a5c239
André Adriano <andre@dev-next.com>
2020-08-19 17:34:50 -0300

    :construction: Create base for total page

---
+++ pages/total.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="total gradient-background">
+    <div class="total__message">
+      <h1>Total</h1>
+    </div>
+    <div class="total__actions">
+      <button class="button" @click="$router.push('/')">Voltar</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import gsap from 'gsap'
+export default {
+  mounted() {
+    gsap.fromTo(
+      '.total *',
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+      }
+    )
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.total {
+  height: calc(100vh - 10rem);
+  padding: 5rem;
+  color: #fff;
+}
+</style>
