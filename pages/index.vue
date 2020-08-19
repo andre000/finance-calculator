90836dcbf13b514a072d1c22e262cace7f74a8a2
André Adriano <andre@dev-next.com>
2020-08-19 17:34:30 -0300

    :dizzy: Add index page animation

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
-<template :key="$route.fullPath">
-  <div class="index">
-    <div class="index__head">
+<template>
+  <div class="index enter">
+    <div class="index__head gradient-background">
       <h1 class="index__head__title">Simulador Financeiro</h1>
       <span class="index__head__description">
         Insira um valor mensal e o tempo de investimento
@@ -14,6 +14,7 @@
 
 <script>
 import { mapMutations } from 'vuex'
+import gsap from 'gsap'
 
 export default {
   mounted() {
@@ -24,11 +25,79 @@ export default {
     })
 
     this.SET_TOTAL(0)
+    this.animateEnter()
   },
 
   methods: {
-    handleSubmit(form) {
+    async handleSubmit(form) {
+      await this.animateLeave()
       this.SET_FORM(form)
+      this.$router.push('/total')
+    },
+
+    animateEnter() {
+      const page = document.querySelector('.index')
+      const head = document.querySelector('.index__head')
+      const body = document.querySelector('.index__body')
+
+      gsap.fromTo(
+        page,
+        {
+          gridTemplateColumns: '100vw 0vw',
+        },
+        {
+          gridTemplateColumns: '33vw 77vw',
+          duration: 1,
+          delay: 1,
+        }
+      )
+
+      gsap.fromTo(
+        [...head.children, ...body.children],
+        {
+          opacity: 0,
+        },
+        {
+          opacity: 1,
+          delay: 2,
+          onComplete() {
+            page.classList.remove('enter')
+          },
+        }
+      )
+    },
+
+    animateLeave() {
+      const page = document.querySelector('.index')
+      const head = document.querySelector('.index__head')
+      const body = document.querySelector('.index__body')
+
+      return new Promise((resolve) => {
+        gsap.fromTo(
+          page,
+          {
+            gridTemplateColumns: '33vw 77vw',
+          },
+          {
+            gridTemplateColumns: '100vw 0vw',
+            duration: 1,
+            delay: 0.5,
+            onComplete() {
+              resolve(true)
+            },
+          }
+        )
+
+        gsap.fromTo(
+          [...head.children, ...body.children],
+          {
+            opacity: 1,
+          },
+          {
+            opacity: 0,
+          }
+        )
+      })
     },
     ...mapMutations(['SET_FORM', 'SET_TOTAL']),
   },
@@ -40,30 +109,35 @@ export default {
 
 .index {
   display: grid;
-  grid-template-columns: 0.33fr 0.77fr;
+  grid-template-columns: 33vw 77vw;
   grid-template-rows: 1fr;
   gap: 1px 1px;
   grid-template-areas: 'head body';
   min-height: 100vh;
+  overflow: hidden;
+
+  &.enter {
+    grid-template-columns: 100vw 0vw;
+  }
 
   &__head {
     grid-area: head;
     padding: 5rem;
     color: #ffffff;
-    background: linear-gradient(245deg, #2e8ce4, #5ccef0, #5c8df0);
-    background-size: 600% 600%;
-
-    -webkit-animation: AnimatedBackground 30s ease infinite;
-    -moz-animation: AnimatedBackground 30s ease infinite;
-    animation: AnimatedBackground 30s ease infinite;
 
     &__title {
+      .enter & {
+        opacity: 0;
+      }
       margin: 0px 0px 1.5rem;
       font-size: 2.5rem;
       font-weight: 900;
     }
 
     &__description {
+      .enter & {
+        opacity: 0;
+      }
       color: #eeeeee;
       font-weight: 100;
     }
@@ -74,38 +148,5 @@ export default {
     grid-area: body;
   }
 }
-
-@-webkit-keyframes AnimatedBackground {
-  0% {
-    background-position: 0% 84%;
-  }
-  50% {
-    background-position: 100% 17%;
-  }
-  100% {
-    background-position: 0% 84%;
-  }
-}
-@-moz-keyframes AnimatedBackground {
-  0% {
-    background-position: 0% 84%;
-  }
-  50% {
-    background-position: 100% 17%;
-  }
-  100% {
-    background-position: 0% 84%;
-  }
-}
-@keyframes AnimatedBackground {
-  0% {
-    background-position: 0% 84%;
-  }
-  50% {
-    background-position: 100% 17%;
-  }
-  100% {
-    background-position: 0% 84%;
-  }
-}
 </style>
+;
