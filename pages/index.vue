29316ee64162c2e1a284c737ac3f9321408dbc4e
André <andre@dev-next.com>
2020-08-20 00:46:51 -0300

    📱 Fix animation on smaller screens

--- pages/index.vue
+++ pages/index.vue
@@ -40,17 +40,21 @@ export default {
       const head = document.querySelector('.index__head')
       const body = document.querySelector('.index__body')
 
-      gsap.fromTo(
-        page,
-        {
-          gridTemplateColumns: '100vw 0vw',
-        },
-        {
-          gridTemplateColumns: '33vw 77vw',
-          duration: 1,
-          delay: 1,
-        }
-      )
+      const transitionEnter =
+        window.screen.width > 480
+          ? { gridTemplateColumns: '100vw 0vw' }
+          : { gridTemplateRows: '100vh 0vh' }
+
+      const transitionLeave =
+        window.screen.width > 480
+          ? { gridTemplateColumns: '33vw 77vw' }
+          : { gridTemplateRows: '4fr 6fr' }
+
+      gsap.fromTo(page, transitionEnter, {
+        ...transitionLeave,
+        duration: 1,
+        delay: 1,
+      })
 
       gsap.fromTo(
         [...head.children, ...body.children],
@@ -62,6 +66,7 @@ export default {
           delay: 2,
           onComplete() {
             page.classList.remove('enter')
+            page.removeAttribute('style')
           },
         }
       )
@@ -72,21 +77,27 @@ export default {
       const head = document.querySelector('.index__head')
       const body = document.querySelector('.index__body')
 
+      const transitionLeave =
+        window.screen.width > 480
+          ? { gridTemplateColumns: '100vw 0vw' }
+          : { gridTemplateRows: '100vh 0vh' }
+
+      const transitionEnter =
+        window.screen.width > 480
+          ? { gridTemplateColumns: '33vw 77vw' }
+          : { gridTemplateRows: '4fr 6fr' }
+
       return new Promise((resolve) => {
-        gsap.fromTo(
-          page,
-          {
-            gridTemplateColumns: '33vw 77vw',
+        gsap.fromTo(page, transitionEnter, {
+          ...transitionLeave,
+          duration: 1,
+          delay: 0.5,
+          clearProps: 'all',
+          onComplete() {
+            page.removeAttribute('style')
+            resolve(true)
           },
-          {
-            gridTemplateColumns: '100vw 0vw',
-            duration: 1,
-            delay: 0.5,
-            onComplete() {
-              resolve(true)
-            },
-          }
-        )
+        })
 
         gsap.fromTo(
           [...head.children, ...body.children],
@@ -116,14 +127,38 @@ export default {
   min-height: 100vh;
   overflow: hidden;
 
+  @media #{$screen-xs} {
+    overflow-y: auto;
+    grid-template-columns: 100vw;
+    grid-template-rows: 4fr 6fr;
+    grid-template-areas:
+      'head'
+      'body';
+  }
+
   &.enter {
     grid-template-columns: 100vw 0vw;
+    @media #{$screen-xs} {
+      grid-template-rows: 100vh 0vh;
+    }
   }
 
   &__head {
     grid-area: head;
     padding: 5rem;
     color: #ffffff;
+
+    @media #{$screen-sm} {
+      padding: 5rem 1rem;
+    }
+
+    @media #{$screen-xs} {
+      padding: 2rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+    }
 
     &__title {
       .enter & {
@@ -146,6 +181,14 @@ export default {
   &__body {
     padding: 5rem;
     grid-area: body;
+
+    @media #{$screen-xs} {
+      padding: 2rem;
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+      margin-bottom: 1rem;
+    }
   }
 }
 </style>
