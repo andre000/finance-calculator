b5e066f8120ffde03d51f843af313e51a6eb65e7
André <andre@dev-next.com>
2020-08-20 00:47:10 -0300

    ✨ Create total page component

--- pages/total.vue
+++ pages/total.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="total gradient-background">
     <div class="total__message">
-      <h1>Total</h1>
-      {{ total }}
+      <h1>Olá {{ form.name.length > 0 ? form.name : 'estranho' }}!</h1>
+      <p>
+        Juntando {{ currency(form.monthPayment) }} todo mês, você terá
+        {{ currency(total) }} em {{ form.time }} anos
+      </p>
     </div>
     <div class="total__actions">
       <button class="button" @click="$router.push('/')">Voltar</button>
@@ -13,25 +16,20 @@
 <script>
 import gsap from 'gsap'
 import { mapActions, mapState } from 'vuex'
+import { currency } from '@/utils/format'
+
 export default {
-  mounted() {
-    this.getTotal()
-    gsap.fromTo(
-      '.total *',
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-      }
-    )
+  computed: {
+    ...mapState(['total', 'form']),
   },
 
-  computed: {
-    ...mapState(['total']),
+  mounted() {
+    this.getTotal()
+    gsap.fromTo('.total *', { opacity: 0 }, { opacity: 1 })
   },
 
   methods: {
+    currency,
     ...mapActions(['getTotal']),
   },
 
@@ -42,9 +40,35 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '@/assets/scss/variables.scss';
+
 .total {
   height: calc(100vh - 10rem);
   padding: 5rem;
   color: #fff;
+
+  @media #{$screen-xs} {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: calc(100vh);
+    padding: 0rem 3rem;
+
+    h1 {
+      margin-top: 0px;
+    }
+  }
+
+  &__actions {
+    button {
+      background: #fffd;
+      color: $primary-color;
+
+      @media #{$screen-xs} {
+        margin-top: 5rem;
+      }
+    }
+  }
 }
 </style>
